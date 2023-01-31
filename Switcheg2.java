0def54f032f136aef27496a4630f5aa3568094b3
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-01-31 20:19:29 +0530

    Add files via upload

---
+++ Switcheg2.java
@@ -0,0 +1,34 @@
+package ControlStatement;
+
+public class Switcheg2 {
+	public static void main(String[] args) {
+		String str="Tuesday";
+		switch(str) { //Switch Expression
+		//Case statement (Multiple Condition)
+		case "Sunday":
+			System.out.println("20");
+			break;
+		case "Monday":
+			System.out.println("50");
+			break;
+		case "Tuesday":
+			System.out.println("40");
+			break;
+case "Wednesday":
+			System.out.println("100");
+			break;
+		case "Thursday":
+			System.out.println("50");
+			break;
+		case "Friday":
+			System.out.println("40");
+			break;
+		case "Saturday":
+			System.out.println("100");
+			break;
+		default:System.out.println("Invalid Number");
+
+	}
+}
+
+}

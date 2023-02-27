7ebab71db0589c1c4e81e3fae619584c8de18cad
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-02-27 18:22:14 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,25 @@
+package Today27Feb;
+abstract class Bankaccount1 {
+	int accountnumber=4546463; 
+	int balance=4500;
+	abstract  void deposit(); 
+		abstract void withdraw(); 
+}
+class checkingAccount extends Bankaccount1 {
+	void deposit() {
+		int deposit=1000;
+		System.out.println("case deposit:" +" "+ (balance + deposit) );
+	}
+	void withdraw() {
+		int withdraw=5000;
+		System.out.println("case withdraw:"+" " + (balance-withdraw) );
+	}
+public class BankAccount {
+	public static void main(String[] args) {
+		Bankaccount1 obj=new checkingAccount();
+		
+		 obj.withdraw();
+		 obj.deposit();
+	}
+}
+}

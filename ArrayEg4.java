8a9bc735cdfbf15174c1bb21be874550b6911158
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-03-02 17:53:25 +0530

    Add files via upload

---
+++ ArrayEg4.java
@@ -0,0 +1,9 @@
+package Today2Mar;
+
+public class ArrayEg4 {
+	public static void main(String[] args) {
+		String [] names = {"Gaurav","Rahul","Prince","Om","Ajay","Tushar","Saurav","Sandeep","Sumit","Aryan"};
+	    for (int i =0;i<names.length;i++){
+	    System.out.println(names[i]);	}
+	    }
+}

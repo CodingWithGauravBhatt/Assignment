456143ac6d523f61f5b151f4eb5a2303eef6a9fd
CodingWithGauravBhatt <122785413+CodingWithGauravBhatt@users.noreply.github.com>
2023-04-05 22:28:01 +0530

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -1,20 +1,73 @@
-package Class;
+package Today5Apr;
+//public class Assignment1 {
+class  BankAccount  {
+  int balance;
 
-import java.util.Scanner;
+  public BankAccount (int balance) {
+      this.balance = balance;
+  }
 
-public class Assignment1 {
-		public static void main(String []args){
-	       //Take input from the user
-	       //Create an instance of the Scanner Class
-	       Scanner sc=new Scanner(System.in);
-	       //Declare and Initialize the variable
-	       System.out.println("Enter the number: ");
-	       int num=sc.nextInt();
-	       int i=1,fact=1;
-	       while(i<=num){
-	           fact=fact*i;
-	           i++;
-	       }
-	       System.out.println("Factorial of the number: "+fact);  
-	    } }
+  synchronized void deposit(int amount) {
+      balance += amount;
+      System.out.println("Deposited: " + amount);
+      notify(); 
+      try {
+          wait(); 
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+  }
 
+  synchronized void withdraw(int amount) {
+      if (balance < amount) {
+          try {
+              System.out.println("Waiting for deposit...");
+              wait(); // wait for deposit before withdrawing
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
+      }
+      balance -= amount;
+      System.out.println("Withdrawn: " + amount);
+      notify(); // wake up any waiting thread
+  }
+}
+
+class Depositor extends Thread {
+ BankAccount account;
+
+  public Depositor(BankAccount account) {
+      this.account = account;
+  }
+
+  public void run() {
+      for (int i = 1; i <= 10; i++) {
+          account.deposit(i * 100);
+      }
+  }
+}
+
+class Withdrawer extends Thread {
+  private BankAccount account;
+
+  public Withdrawer(BankAccount account) {
+      this.account = account;
+  }
+
+  public void run() {
+      for (int i = 1; i <= 10; i++) {
+          account.withdraw(i * 50);
+      }
+  }
+}
+
+public class Assignment1{
+  public static void main(String[] args) {
+      BankAccount account = new BankAccount(0);
+      Depositor depositor = new Depositor(account);
+      Withdrawer withdrawer = new Withdrawer(account);
+
+      depositor.start();
+      withdrawer.start();
+  }
+}
